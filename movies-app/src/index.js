84cab2981cb1c1fd4e494ce4dfb9f1e6c463ef9d
Sagar Galwade <sagargalwade04@gmail.com>
2023-02-08 00:15:03 +0530

    Checkpoint 4

--- movies-app/src/index.js
+++ movies-app/src/index.js
@@ -1,23 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import Home from "./screens/home/Home";
-import Details from "./screens/details/Details";
+import ReactDOM from 'react-dom/client';
 import './index.css';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import NotFound from './common/NotFound';
-import 'typeface-roboto';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-ReactDOM.render(
-    <BrowserRouter>
-      {/* <Home /> */}
-      {/* <Details /> */}
-      <Switch>
-        <Route path="/" exact component={Home}/>
-        <Route path="/detail" component={Details}/>
-        <Route component={NotFound}/>
-      </Switch>
-    </BrowserRouter>
-,
-  document.getElementById('root')
-); 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
