2351a6ae260657cd4a1ddbb2b47ee8faeb8dc39d
Sagar Galwade <sagargalwade04@gmail.com>
2023-02-14 14:54:08 +0530

    Checkpoin 1

--- movies-app/src/index.js
+++ movies-app/src/index.js
@@ -4,15 +4,27 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
- 
+// ReactDOM.render(
+//   <BrowserRouter>
+//     {/* <Home /> */}
+//     {/* <Details /> */}
+//     <Switch>
+//       <Route path="/" exact component={Home}/>
+//       <Route path="/detail" component={Details}/>
+//       <Route component={NotFound}/>
+//     </Switch>
+//   </BrowserRouter>
+// ,
+// document.getElementById('root')
+// );
