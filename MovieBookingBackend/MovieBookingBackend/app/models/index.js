01384acaa5b603e837c7cb2a35aa1da8beb79e6f
Sagar Galwade <sagargalwade04@gmail.com>
2023-02-14 14:56:40 +0530

    Checkpoint 3

--- MovieBookingBackend/MovieBookingBackend/app/models/index.js
+++ MovieBookingBackend/MovieBookingBackend/app/models/index.js
@@ -5,10 +5,10 @@ const mongoose = require("mongoose");
 
 const db={};
 db.mongoose = mongoose;
-db.url = dbConfig.url;
-db.users = require("./user.model")(mongoose);
-db.genres = require("./genre.model")(mongoose);
-db.movies = require("./movie.model")(mongoose);
-db.artists = require("./artist.model")(mongoose);
+db.url= dbConfig.url;
+db.users= require("./user.model")(mongoose);
+db.genres= require("./genre.model")(mongoose);
+db.movies= require("./movie.model")(mongoose);
+db.artists= require("./artist.model")(mongoose);
 
 module.exports = db;
